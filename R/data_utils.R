4fb21280f20b9b1e564186b4a9e95eb5e4c0ee8f
Divendra2006 <divendra.singhyadav.phe23@itbhu.ac.in>
2025-06-28 01:07:56 +0530

    replace source() to load() in data_utils.R file

--- R/data_utils.R
+++ R/data_utils.R
@@ -1,40 +1,37 @@
 #' Load Custom Datasets
 #'
-#' Loads datasets from the data-raw folder or uses built-in datasets
+#' Loads datasets from the package data or uses built-in datasets
 #'
 #' @return A list of datasets
 #' @keywords internal
 load_custom_datasets <- function() {
   datasets <- list()
 
-  # Try to load four_clusters.R
   tryCatch({
-    source(system.file("data-raw", "four_clusters.R", package = "nldrViz"), local = TRUE)
-    if(exists("four_clusters")) {
+    load(system.file("data", "four_clusters.rda", package = "nldrViz"), envir = environment())
+    if(exists("four_clusters", envir = environment())) {
       datasets[["four_clusters"]] <- four_clusters
     }
   }, error = function(e) {
-    warning("Could not load four_clusters.R: ", e$message)
+    warning("Could not load four_clusters data: ", e$message)
   })
 
-  # Try to load pdfsense.R
   tryCatch({
-    source(system.file("data-raw", "pdfsense.R", package = "nldrViz"), local = TRUE)
-    if(exists("pdfsense")) {
+    load(system.file("data", "pdfsense.rda", package = "nldrViz"), envir = environment())
+    if(exists("pdfsense", envir = environment())) {
       datasets[["pdfsense"]] <- pdfsense
     }
   }, error = function(e) {
-    warning("Could not load pdfsense.R: ", e$message)
+    warning("Could not load pdfsense data: ", e$message)
   })
 
-  # Try to load trees.R
   tryCatch({
-    source(system.file("data-raw", "trees.R", package = "nldrViz"), local = TRUE)
-    if(exists("fake_trees")) {
+    load(system.file("data", "fake_trees.rda", package = "nldrViz"), envir = environment())
+    if(exists("fake_trees", envir = environment())) {
       datasets[["trees"]] <- fake_trees
     }
   }, error = function(e) {
-    warning("Could not load trees.R: ", e$message)
+    warning("Could not load fake_trees data: ", e$message)
   })
 
   return(datasets)
